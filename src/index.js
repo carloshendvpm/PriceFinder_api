7a952550454d5f4f3bf3ba46d447f99be2d873d9
Carlos Henrique <hencarlosdv@gmail.com>
2023-05-07 14:46:38 -0300

    feat: set default por if it is not defined

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import dotenf from 'dotenv'
+import dotenv from 'dotenv'
 
 dotenv.config()
 
@@ -11,5 +11,5 @@ app.use(express.json())
 
 //require("..src/routes/index")(app)
 
-app.listen(process.env.PORT)
+app.listen(process.env.PORT || 3000)
 console.log('server on port', process.env.PORT)
