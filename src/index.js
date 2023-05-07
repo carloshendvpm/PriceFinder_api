eb2e679e8f900e4dc5a7a74df1657a92dbc942de
Carlos Henrique <hencarlosdv@gmail.com>
2023-05-07 14:44:03 -0300

    refactor: using import instead of require

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
+import express from 'express'
+import cors from 'cors'
+import dotenf from 'dotenv'
 
 dotenv.config()
 
