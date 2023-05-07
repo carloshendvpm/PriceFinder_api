cda53c37853dd3d8b9674b2afdabfce7e72ee375
Carlos Henrique <hencarlosdv@gmail.com>
2023-05-07 15:46:44 -0300

    feat: add bcrypt hash validation to password

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,13 @@
-import { createUser } from "../repositories/user.repository"; 
+import { createUser } from "../repositories/user.repository";
+import { userValidation } from "../validations/user.validation";
+import bcrypt from "bcrypt";
 
 export const create = async (req, res) => {
   try {
+    await userValidation.validate(req.body)
+
+    const hashPass = await bcrypt.hash(req.body.password, 10);
+    req.body.password = hashPass;
     const user = await createUser(req.body);
     res.status(200).send(user);
   } catch(err){
