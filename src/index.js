7090b35d3bd07263ab29c28fb6ef73c3a0c189a6
Carlos Henrique <hencarlosdv@gmail.com>
2023-05-07 14:57:11 -0300

    refactor: using import to routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-
+import routes from './routes'
 dotenv.config()
 
 const app = express();
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
-//require("..src/routes/index")(app)
+routes(app);
 
 app.listen(process.env.PORT || 3000)
 console.log('server on port', process.env.PORT)
