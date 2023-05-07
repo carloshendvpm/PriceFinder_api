3350799655f73c50ff5883dd09df6c2210805e7f
Carlos Henrique <hencarlosdv@gmail.com>
2023-05-07 15:45:24 -0300

    feat: create user only returns necessary data

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -2,7 +2,16 @@ import { prisma } from "../services/prisma";
 
 export const createUser = async(data) => {
   const user = await prisma.user.create({
-    data
+    data,
+    select: {
+      id:true,
+      name:true,
+      email:true,
+      password:false,
+      phone:true,
+      createdAt:true,
+      updatedAt: true
+    }
   })
   return user
-}
+};
