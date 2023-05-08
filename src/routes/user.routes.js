b53445ed7885efe571ee732676d7569bc5713482
Carlos Henrique <hencarlosdv@gmail.com>
2023-05-08 14:20:57 -0300

    feat: add update route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,10 @@
-import { create, get, getId } from "../controllers/user.controller";
+import { create, get, getId, update } from "../controllers/user.controller";
 
 const userRoutes = (app) => {
   app.post("/user", create);
   app.get("/user", get)
   app.get("/user/:id", getId)
+  app.put("/user/:id", update)
 }
 
 export default userRoutes
