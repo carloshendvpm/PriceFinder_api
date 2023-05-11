6ac6d0fb19a3b7a14dc273a6b1dc385e5e2a7d01
Carlos Henrique <hencarlosdv@gmail.com>
2023-05-11 13:36:42 -0300

    feat: created product repository

---
+++ src/repositories/product.repository.ts
@@ -0,0 +1,64 @@
+import { prisma } from "../services/prisma";
+import Product from "../types/product.types";
+
+export const createProduct = async(data: Product) => {
+  const product = await prisma.product.create({
+    data,
+    select: {
+      id:true,
+      name:true,
+      description:true,
+      category:true,
+      image:true
+    }
+  })
+  return product
+};
+export const getAll = async() => {
+  const product = await prisma.product.findMany({
+    select: {
+      id:true,
+      name:true,
+      description:true,
+      category:true,
+      image:true
+    }
+  })
+  return product
+}
+export const getById = async(id: number) => {
+  const product = await prisma.product.findUnique({
+    where: { id },
+    select: {
+      id:true,
+      name:true,
+      description:true,
+      category:true,
+      image:true
+    }
+  })
+  return product
+}
+export const updateProduct = async (id: number, data: Product) => {
+  const product = await prisma.product.update({
+    where: {
+      id,
+    },
+    data,
+    select: {
+      id:true,
+      name:true,
+      description:true,
+      category:true,
+      image:true
+    }
+  })
+  return product
+}
+export const deleteProduct = async (id: number) => {
+  await prisma.product.delete({
+    where: {
+      id,
+    },
+  })
+}
