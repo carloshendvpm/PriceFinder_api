e24dd7bfc57e1211471d7c442ceff9031337bcb2
Carlos Henrique <hencarlosdv@gmail.com>
2023-05-12 02:57:56 -0300

    feat: create crud for category

---
+++ src/controllers/category.controller.ts
@@ -0,0 +1,46 @@
+import { createCategory, getAll, getById, updateCategory, deleteCategory } from "../repositories/category.repository";
+import { Request, Response } from "express";
+
+export const create = async (req: Request, res: Response) => {
+  try {
+    const category = await createCategory(req.body);
+    res.status(200).send(category);
+  } catch(err){
+    res.status(400).send(err);
+  }
+}
+
+export const get = async (req: Request, res: Response) => {
+  try{
+    const products = await getAll()
+    res.status(200).send(products)
+  }catch(err){
+    res.status(400).send(err)
+  }
+}
+
+export const getId = async (req: Request, res: Response) => {
+  try {
+    const category = await getById(Number(req.params.id))
+    res.status(200).send(category)
+  } catch(err) {
+    res.status(400).send(err)
+  }
+}
+
+export const update = async (req: Request, res: Response) => {
+  try {
+    const category = await updateCategory(Number(req.params.id), req.body)
+    res.status(200).send(category)
+  } catch(err) {
+    res.status(400).send(err)
+  }
+}
+export const remove = async (req: Request, res: Response) => {
+  try {
+    await deleteCategory(Number(req.params.id))
+    res.status(200).send()
+  } catch(err) {
+    res.status(400).send(err)
+  }
+}
