702ad19a9f9384ae737794c773df55c46c630cae
Carlos Henrique <hencarlosdv@gmail.com>
2023-05-15 16:26:57 -0300

    test: add new tests

--- __tests__/product.controller.test.ts
+++ __tests__/product.controller.test.ts
@@ -50,4 +50,88 @@ describe("ProductController", () => {
       expect(res.send).toHaveBeenCalledWith(error);
     });
   });
+  describe("get", () => {
+    it("should get all products", async () => {
+      const products = [
+        { id: 1, name: "Product 1", description: "Product description" },
+        { id: 2, name: "Product 2", description: "Product description" }
+      ];
+      (ProductService.getAll as jest.Mock).mockResolvedValue(products);
+
+      await ProductController.get(req, res);
+
+      expect(ProductService.getAll).toHaveBeenCalled();
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.send).toHaveBeenCalledWith(products);
+    });
+
+    it("should handle errors during get", async () => {
+      const error = new Error("Get error");
+      (ProductService.getAll as jest.Mock).mockRejectedValue(error);
+
+      await ProductController.get(req, res);
+
+      expect(ProductService.getAll).toHaveBeenCalled();
+      expect(res.status).toHaveBeenCalledWith(400);
+      expect(res.send).toHaveBeenCalledWith(error);
+    });
+  });
+
+  describe("getById", () => {
+    it("should get product by ID", async () => {
+      const productId = 1;
+      const product = { id: productId, name: "Product 1", description: "Product description" };
+      (ProductService.getById as jest.Mock).mockResolvedValue(product);
+      req.params = { id: productId.toString() };
+
+      await ProductController.getById(req, res);
+
+      expect(ProductService.getById).toHaveBeenCalledWith(productId);
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.send).toHaveBeenCalledWith(product);
+    });
+
+    it("should handle errors during getById", async () => {
+      const productId = 1;
+      const error = new Error("GetById error");
+      (ProductService.getById as jest.Mock).mockRejectedValue(error);
+      req.params = { id: productId.toString() };
+
+      await ProductController.getById(req, res);
+
+      expect(ProductService.getById).toHaveBeenCalledWith(productId);
+      expect(res.status).toHaveBeenCalledWith(400);
+      expect(res.send).toHaveBeenCalledWith(error);
+    });
+  });
+
+  describe("update", () => {
+    it("should update a product", async () => {
+      const productId = 1;
+      const updatedProduct = { id: productId, name: "Updated Product", description: "Updated description" };
+      (ProductService.updateProduct as jest.Mock).mockResolvedValue(updatedProduct);
+      req.params = { id: productId.toString() };
+      req.body = { name: "Updated Product", description: "Updated description" };
+
+      await ProductController.update(req, res);
+
+      expect(ProductService.updateProduct).toHaveBeenCalledWith(productId, req.body);
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.send).toHaveBeenCalledWith(updatedProduct);
+    });
+
+    it("should handle errors during update", async () => {
+      const productId = 1;
+      const error = new Error("Update error");
+      (ProductService.updateProduct as jest.Mock).mockRejectedValue(error);
+      req.params = { id: productId.toString() };
+      req.body = { name: "Updated Product", description: "Updated description" };
+
+      await ProductController.update(req, res);
+
+      expect(ProductService.updateProduct).toHaveBeenCalledWith(productId, req.body);
+      expect(res.status).toHaveBeenCalledWith(400);
+      expect(res.send).toHaveBeenCalledWith(error);
+    });
+  });
 });
