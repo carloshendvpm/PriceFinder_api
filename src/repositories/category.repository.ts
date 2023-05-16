9670983b524c4d7b3fdaa53d0c86c4ffda7f338f
Carlos Henrique <hencarlosdv@gmail.com>
2023-05-16 09:35:29 -0300

    refactor: using classes in category

--- src/repositories/category.repository.ts
+++ src/repositories/category.repository.ts
@@ -1,35 +1,44 @@
 import { prisma } from "../services/prisma";
 import Category from "../interfaces/category.interface";
 
-export const createCategory = async(data: Category) => {
-  const category = await prisma.category.create({
-    data,
-  })
-  return category
+
+class CategoryService {
+  async createCategory(data: Category) {
+    const category = await prisma.category.create({
+      data,
+    });
+    return category;
+  }
+
+  async getAll() {
+    const categories = await prisma.category.findMany();
+    return categories;
+  }
+
+  async getById(id: number) {
+    const category = await prisma.category.findUnique({
+      where: { id },
+    });
+    return category;
+  }
+
+  async updateCategory(id: number, data: Category) {
+    const category = await prisma.category.update({
+      where: {
+        id,
+      },
+      data,
+    });
+    return category;
+  }
+
+  async deleteCategory(id: number) {
+    await prisma.category.delete({
+      where: {
+        id,
+      },
+    });
+  }
 }
-export const getAll = async() => {
-  const categories = await prisma.category.findMany();
-  return categories
-}
-export const getById = async(id: number) => {
-  const category = await prisma.category.findUnique({
-    where: { id },
-  })
-  return category
-}
-export const updateCategory = async (id: number, data: Category) => {
-  const category = await prisma.category.update({
-    where: {
-      id,
-    },
-    data,
-  })
-  return category
-}
-export const deleteCategory = async (id: number) => {
-  await prisma.category.delete({
-    where: {
-      id,
-    }
-  })
-}
+
+export default new CategoryService();
