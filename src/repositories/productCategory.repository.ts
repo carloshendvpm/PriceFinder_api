d1d707beb95341581e0d9272eafca828657b715e
Carlos Henrique <hencarlosdv@gmail.com>
2023-05-16 10:07:14 -0300

    refactor: using classes in productCategory

--- src/repositories/productCategory.repository.ts
+++ src/repositories/productCategory.repository.ts
@@ -1,9 +1,13 @@
 import { prisma } from "../services/prisma";
 import ProductCategory from "../interfaces/productCategory.interface";
 
-export const createProductCategory = async (data: ProductCategory) => {
-  const productCategory = await prisma.productCategory.create({
-    data,
-  });
-  return productCategory;
+class ProductCategoryService {
+  async createProductCategory(data: ProductCategory) {
+    const productCategory = await prisma.productCategory.create({
+      data,
+    });
+    return productCategory;
+  }
 }
+
+export default new ProductCategoryService();
