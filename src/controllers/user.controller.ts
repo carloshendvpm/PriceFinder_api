8ad9bdd1096f46282c2c7f30eaa8bd71f82b0399
Carlos Henrique <hencarlosdv@gmail.com>
2023-05-16 10:06:25 -0300

    refactor: using classes in users

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,53 +1,63 @@
-import { createUser, getAll, getById, updateUser, deleteUser } from "../repositories/user.repository";
+import UserService from "../repositories/user.repository";
 import { userValidation } from "../validations/user.validation";
 import bcrypt from "bcrypt";
 import { Request, Response } from "express";
 
-export const create = async (req: Request, res: Response) => {
-  try {
-    await userValidation.validate(req.body);
+class UserController {
+  async create(req: Request, res: Response) {
+    try {
+      await userValidation.validate(req.body);
 
+      const hashPass = await bcrypt.hash(req.body.password, 10);
+      req.body.password = hashPass;
 
-    const hashPass = await bcrypt.hash(req.body.password, 10);
-    req.body.password = hashPass;
-    const user = await createUser(req.body);
-    res.status(200).send(user);
-  } catch(err){
-    res.status(400).send(err);
+      const user = await UserService.createUser(req.body);
+      res.status(200).send(user);
+    } catch (err) {
+      res.status(400).send(err);
+    }
+  }
+
+  async get(req: Request, res: Response) {
+    try {
+      const users = await UserService.getAll();
+      res.status(200).send(users);
+    } catch (err) {
+      res.status(400).send(err);
+    }
+  }
+
+  async getById(req: Request, res: Response) {
+    try {
+      const userId = Number(req.params.id);
+      const user = await UserService.getById(userId);
+      res.status(200).send(user);
+    } catch (err) {
+      res.status(400).send(err);
+    }
+  }
+
+  async update(req: Request, res: Response) {
+    try {
+      const userId = Number(req.params.id);
+      const updatedUser = req.body;
+
+      const user = await UserService.updateUser(userId, updatedUser);
+      res.status(200).send(user);
+    } catch (err) {
+      res.status(400).send(err);
+    }
+  }
+
+  async remove(req: Request, res: Response) {
+    try {
+      const userId = Number(req.params.id);
+      await UserService.deleteUser(userId);
+      res.status(200).send();
+    } catch (err) {
+      res.status(400).send(err);
+    }
   }
 }
 
-export const get = async (req: Request, res: Response) => {
-  try{
-    const users = await getAll()
-    res.status(200).send(users)
-  }catch(err){
-    res.status(400).send(err)
-  }
-}
-
-export const getId = async (req: Request, res: Response) => {
-  try {
-    const user = await getById(Number(req.params.id))
-    res.status(200).send(user)
-  } catch(err) {
-    res.status(400).send(err)
-  }
-}
-
-export const update = async (req: Request, res: Response) => {
-  try {
-    const user = await updateUser(Number(req.params.id), req.body)
-    res.status(200).send(user)
-  } catch(err) {
-    res.status(400).send(err)
-  }
-}
-export const remove = async (req: Request, res: Response) => {
-  try {
-    await deleteUser(Number(req.params.id))
-    res.status(200).send()
-  } catch(err) {
-    res.status(400).send(err)
-  }
-}
+export default new UserController();
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,12 +1,12 @@
-import { create, get, getId, update, remove } from "../controllers/user.controller";
-import { verifyToken } from "../middlewares/auth";
+import UserController from "../controllers/user.controller";
+import auth from "../middlewares/auth";
 
 const userRoutes = (app: any) => {
-  app.post("/user", create);
-  app.get("/user", verifyToken, get)
-  app.get("/user/:id",verifyToken, getId)
-  app.put("/user/:id", verifyToken,update)
-  app.delete("/user/:id",verifyToken, remove)
+  app.post("/user", UserController.create);
+  app.get("/user", auth.verifyToken, UserController.get);
+  app.get("/user/:id",auth.verifyToken, UserController.getById);
+  app.put("/user/:id", auth.verifyToken, UserController.update);
+  app.delete("/user/:id",auth.verifyToken, UserController.remove);
 }
 
 export default userRoutes
